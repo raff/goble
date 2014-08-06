7240a11d315d7f666a4e45e1dc24a2610f03b9e9
Raffaele Sena <raff367@gmail.com>
2014-08-06 15:27:35 -0700

    reworked emitter so that callbacks are NOT executing in the context of XPC handlers

--- emitter.go
+++ emitter.go
@@ -19,27 +19,34 @@ type EventHandlerFunc func(Event)
 
 type Emitter struct {
 	handlers map[string]EventHandlerFunc
+	event    chan Event
+}
+
+func (e *Emitter) Init() {
+	e.handlers = make(map[string]EventHandlerFunc)
+	e.event = make(chan Event)
+
+	// event handler
+	go func() {
+		for {
+			ev := <-e.event
+
+			if fn, ok := e.handlers[ev.Name]; ok {
+				fn(ev)
+			} else if fn, ok := e.handlers[ALL]; ok {
+				fn(ev)
+			} else {
+				log.Println("unhandled Emit", ev)
+			}
+		}
+	}()
 }
 
 func (e *Emitter) Emit(ev Event) {
-	if e.handlers == nil {
-		e.handlers = make(map[string]EventHandlerFunc)
-	}
-
-	if fn, ok := e.handlers[ev.Name]; ok {
-		fn(ev)
-	} else if fn, ok := e.handlers[ALL]; ok {
-		fn(ev)
-	} else {
-		log.Println("unhandled emit", ev)
-	}
+	e.event <- ev
 }
 
 func (e *Emitter) On(event string, fn EventHandlerFunc) {
-	if e.handlers == nil {
-		e.handlers = make(map[string]EventHandlerFunc)
-	}
-
 	if fn == nil {
 		delete(e.handlers, event)
 	} else {
