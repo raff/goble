5a206277e7359d09af80eb4519b8fa331f5ac7da
Raffaele Sena <raff367@gmail.com>
2019-02-27 22:30:54 -0800

    More compact messaging

--- examples/discoverer.go
+++ examples/discoverer.go
@@ -10,8 +10,11 @@ import (
 
 func main() {
 	verbose := flag.Bool("verbose", false, "dump all events")
+	compact := flag.Bool("compact", true, "compact messages")
 	dups := flag.Bool("allow-duplicates", false, "allow duplicates when scanning")
 	flag.Parse()
+
+	var quit chan bool
 
 	ble := goble.New()
 	ble.SetVerbose(*verbose)
@@ -29,35 +32,66 @@ func main() {
 		} else {
 			ble.StopScanning()
 			done = true
+			quit <- true
 		}
 
 		return
 	})
 
 	ble.On("discover", func(ev goble.Event) (done bool) {
-		fmt.Println()
-		fmt.Println("peripheral discovered (", ev.DeviceUUID, "):")
-		fmt.Println("\thello my local name is:")
-		fmt.Println("\t\t", ev.Peripheral.Advertisement.LocalName)
-		fmt.Println("\tcan I interest you in any of the following advertised services:")
-		fmt.Println("\t\t", ev.Peripheral.Advertisement.ServiceUuids)
+		if *compact {
+			fmt.Println("peripheral:", ev.DeviceUUID)
+			if ev.Peripheral.Advertisement.LocalName != "" {
+				fmt.Println("  name:", ev.Peripheral.Advertisement.LocalName)
+			}
+			if len(ev.Peripheral.Advertisement.ServiceUuids) > 0 {
+				fmt.Println("  services:", ev.Peripheral.Advertisement.ServiceUuids)
+			}
+		} else {
+			fmt.Println()
+			fmt.Println("peripheral discovered (", ev.DeviceUUID, "):")
+			fmt.Println("\thello my local name is:")
+			fmt.Println("\t\t", ev.Peripheral.Advertisement.LocalName)
+			fmt.Println("\tcan I interest you in any of the following advertised services:")
+			fmt.Println("\t\t", ev.Peripheral.Advertisement.ServiceUuids)
+		}
 
 		serviceData := ev.Peripheral.Advertisement.ServiceData
 		if len(serviceData) > 0 {
-			fmt.Println("\there is my service data:")
+			prefix := "\t\t"
+
+			if *compact {
+				prefix = "    "
+				fmt.Println("  service data:")
+			} else {
+				fmt.Println("\there is my service data:")
+			}
+
 			for _, d := range serviceData {
-				fmt.Println("\t\t", d.Uuid, ":", d.Data)
+				fmt.Println(prefix, d.Uuid, ":", d.Data)
 			}
 		}
 
 		if len(ev.Peripheral.Advertisement.ManufacturerData) > 0 {
-			fmt.Println("\there is my manufacturer data:")
-			fmt.Println("\t\t", ev.Peripheral.Advertisement.ManufacturerData)
+			if *compact {
+				fmt.Println("  manufacturer data:", ev.Peripheral.Advertisement.ManufacturerData)
+			} else {
+				fmt.Println("\there is my manufacturer data:")
+				fmt.Println("\t\t", ev.Peripheral.Advertisement.ManufacturerData)
+			}
 		}
 
 		if ev.Peripheral.Advertisement.TxPowerLevel != 0 {
-			fmt.Println("\tmy TX power level is:")
-			fmt.Println("\t\t", ev.Peripheral.Advertisement.TxPowerLevel)
+			if *compact {
+				fmt.Println("  TX power level:", ev.Peripheral.Advertisement.TxPowerLevel)
+			} else {
+				fmt.Println("\tmy TX power level is:")
+				fmt.Println("\t\t", ev.Peripheral.Advertisement.TxPowerLevel)
+			}
+		}
+
+		if *compact {
+			fmt.Println()
 		}
 
 		return
@@ -69,6 +103,5 @@ func main() {
 
 	ble.Init()
 
-	var done chan bool
-	<-done
+	<-quit
 }
